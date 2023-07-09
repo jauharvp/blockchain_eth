03ef980ce5af35671e795d824a2c3328e54a0039
jauharvp <jauharvp@gmmail.com>
2023-07-09 17:58:27 +0530

    MultiSwig Wallet 1 submit Transaction

--- Applications/MultiSigWallet.sol
+++ Applications/MultiSigWallet.sol
@@ -25,4 +25,71 @@ contract MultiSwigWallet {
         bool executed;
         uint numConfirmations;
     }
+
+    // mapping from tx index => owner => bool
+
+    mapping(uint => mapping (address => bool)) public isConfirmed;
+
+    Transactoin[] public transactions;
+
+    modifier  onlyOwner(){
+        require(isOwner[msg.sender], "not owner");
+    _;
+    }
+
+    modifier txExists(uint _txIndex) {
+        require(_txIndex < transactions.length, "tx does not exist");
+        _;
+    }
+
+    modifier  notExecuted(uint _txIndex){
+        require(!transactions[_txIndex].executed, "tx already executed");
+        _;
+    }
+    modifier notConfirmed(uint _txIndex){
+        require(!isConfirmed[_txIndex][msg.sender],"tx already confirmed");
+        _;
+    }
+
+    constructor(address[] memory _owners, uint _numConfirmationsRequired){
+        require(_owners.length > 0, "Owners required");
+        require(_numConfirmationsRequired > 0 && _numConfirmationsRequired <= _owners.length,"invalid number of required confirmations");
+
+        for (uint i =0; i < _owners.length; i++) 
+        {
+            address owner = _owners[i];
+
+            require(owner != address(0), "inavlid owner");
+            require(!isOwner[owner],"owner not unique");
+
+            isOwner[owner] = true;
+            owners.push(owner);
+
+        }
+
+        numConfirmationsRequired = _numConfirmationsRequired;
+    }
+
+    receive() external  payable {
+        emit Deposit(msg.sender, msg.value, address(this).balance);
+    }
+
+    function submitTransaction(
+        address _to,
+        uint _value,
+        bytes memory _data
+    ) public  onlyOwner {
+        uint txIndex = transactions.length;
+
+        transactions.push(
+            Transactoin({
+                to: _to,
+                value: _value,
+                data: _data,
+                executed: false,
+                numConfirmations: 0
+            })
+        );
+        emit SubmitTransaction(msg.sender, txIndex, _to, _value, _data);
+    }
 }
